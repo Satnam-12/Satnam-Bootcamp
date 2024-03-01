5427961d15a98550e66517327797ddd93eee1eda
Satnam Singh Nazran <89491668+Satnam-12@users.noreply.github.com>
2024-03-01 16:33:08 +0000

    gagana na uli

--- dapp/app/page.tsx
+++ dapp/app/page.tsx
@@ -5,21 +5,22 @@ import { useEffect, useState } from "react";
 import { getContract } from "../config";
 
 export default function Home() {
-  const [walletKey, setwalletKey] = useState("");
-  const [currentData, setcurrentData] = useState("");
+  const [walletKey, setWalletKey] = useState("");
+  const [currentData, setCurrentData] = useState("");
 
   const connectWallet = async () => {
     const { ethereum } = window as any;
     const accounts = await ethereum.request({
       method: "eth_requestAccounts",
     });
-    setwalletKey(accounts[0]);
+    setWalletKey(accounts[0]);
   };
+
   //<Minting>
   const [mintingAmount, setMintingAmount] = useState<number>();
   const [submitted, setSubmitted] = useState(false);
   const [transactionHash, setTransactionHash] = useState("");
-  
+
   const mintCoin = async () => {
     const { ethereum } = window as any;
     const provider = new BrowserProvider(ethereum);
@@ -35,6 +36,7 @@ export default function Home() {
       alert(`Minting failed: ${decodedError?.args}`);
     }
   };
+
   const mintAmountChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const inputValue = e.target.value;
     if (!isNaN(Number(inputValue))) {
@@ -48,6 +50,7 @@ export default function Home() {
 
   //<Staking>
   const [stakingAmount, setStakingAmount] = useState<number>();
+
   const stakeCoin = async () => {
     const { ethereum } = window as any;
     const provider = new BrowserProvider(ethereum);
@@ -60,9 +63,10 @@ export default function Home() {
       setTransactionHash(tx.hash);
     } catch (e: any) {
       const decodedError = contract.interface.parseError(e.data);
-      alert(`Minting failed: ${decodedError?.args}`);
+      alert(`Staking failed: ${decodedError?.args}`);
     }
   };
+
   const stakeAmountChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const inputValue = e.target.value;
     if (!isNaN(Number(inputValue))) {
@@ -73,7 +77,7 @@ export default function Home() {
     }
   };
   //</Staking>
- 
+
   //<Withdraw>
   const withdrawCoin = async () => {
     const { ethereum } = window as any;
@@ -87,19 +91,21 @@ export default function Home() {
       setTransactionHash(tx.hash);
     } catch (e: any) {
       const decodedError = contract.interface.parseError(e.data);
-      alert(`Minting failed: ${decodedError?.args}`);
+      alert(`Withdrawal failed: ${decodedError?.args}`);
     }
   };
   //</Withdraw>
+
   //<Import Token>
-  const importToken = async() => {
-    const {ethereum} = window as any;
+  const importToken = async () => {
+    const { ethereum } = window as any;
     const tokenAddress = "0xe541D317f761599b5B01Af140C438Dd301f308f3";
     const tokenSymbol = "DRA";
     const tokenDecimal = 18;
-    const tokenImage = "https://static.hiphopdx.com/2023/12/drake-introduces-his-newest-alter-ego-complete-with-its-own-accesory-1200x675.jpg";
+    const tokenImage =
+      "https://static.hiphopdx.com/2023/12/drake-introduces-his-newest-alter-ego-complete-with-its-own-accesory-1200x675.jpg";
 
-    try{
+    try {
       const wasAdded = await ethereum.request({
         method: "wallet_watchAsset",
         params: {
@@ -112,82 +118,103 @@ export default function Home() {
           },
         },
       });
-    }
-    catch(error){
+    } catch (error) {
       console.log(error);
     }
   };
   //</Import Token>
 
   return (
-    <main style={{ textAlign: 'center', padding: '20px', fontFamily: 'Arial, sans-serif', backgroundImage: 'url("https://i.wpimg.pl/1200x788/filerepo.grupawp.pl/api/v1/display/embed/5a754f28-670a-4666-a5f4-93dc4bca1d1f")', backgroundSize: 'cover', minHeight: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'center', backdropFilter: 'blur(10px)' }}>
-      <p style={{ fontSize: '40px', fontWeight: 'bold', marginBottom: '20px', color: '#000000' }}>
+    <main
+      style={{
+        textAlign: "center",
+        padding: "20px",
+        fontFamily: "Arial, sans-serif",
+        backgroundImage:
+          'url("https://i.wpimg.pl/1200x788/filerepo.grupawp.pl/api/v1/display/embed/5a754f28-670a-4666-a5f4-93dc4bca1d1f")',
+        backgroundSize: "cover",
+        minHeight: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        backdropFilter: "blur(10px)",
+      }}
+    >
+      <p style={{ fontSize: "40px", fontWeight: "bold", marginBottom: "20px", color: "#000000" }}>
         Drake Mint/Staking (Anita Max Wynn)
       </p>
-  
-      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+
+      <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center" }}>
         <button
           onClick={connectWallet}
           className="p-3 bg-black text-white rounded"
-          style={{ marginBottom: '10px', border: '10px solid #000000' }}
+          style={{ marginBottom: "10px", border: "10px solid #000000" }}
         >
           {walletKey !== "" ? walletKey : "Connect Wallet"}
         </button>
-  
+
         <button
           onClick={importToken}
           className="p-3 bg-black text-white rounded"
-          style={{ border: '2px solid #000000' }}
+          style={{ border: "2px solid #000000" }}
         >
           Import Token
         </button>
       </div>
-  
-      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', border: '2px solid #000000', padding: '10px', marginBottom: '20px' }}>
+
+      <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", border: "2px solid #000000", padding: "10px", marginBottom: "20px" }}>
         <form>
-          <label style={{ fontSize: '18px', color: '#000000' }}>Ready to mint some tokens? Enter the amount you want to create and let the magic happen! 💎✨</label><br />
+          <label style={{ fontSize: "18px", color: "#000000" }}>
+            Ready to mint some tokens? Enter the amount you want to create and let the magic happen! 💎✨
+          </label>
+          <br />
         </form>
         <input
           type="number"
           value={mintingAmount}
           onChange={(e) => mintAmountChange(e)}
-          style={{ color: 'black', marginBottom: '10px', padding: '8px', fontSize: '16px', border: '1px solid #000000' }}
+          style={{ color: "black", marginBottom: "10px", padding: "8px", fontSize: "16px", border: "1px solid #000000" }}
         />
         <button
           onClick={mintCoin}
           className="p-3 bg-black text-white rounded"
-          style={{ border: '2px solid #000000' }}
+          style={{ border: "2px solid #000000" }}
         >
           Mint Token
         </button>
       </div>
-  
-      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', border: '2px solid #000000', padding: '10px', marginBottom: '20px' }}>
+
+      <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", border: "2px solid #000000", padding: "10px", marginBottom: "20px" }}>
         <form>
-          <label style={{ fontSize: '18px', color: '#000000' }}>Input the amount you'd like to stake right here and watch your investment grow! 💰🌱</label><br />
+          <label style={{ fontSize: "18px", color: "#000000" }}>
+            Input the amount you'd like to stake right here and watch your investment grow! 💰🌱
+          </label>
+          <br />
         </form>
         <input
           type="number"
           value={stakingAmount}
           onChange={(e) => stakeAmountChange(e)}
-          style={{ color: 'black', marginBottom: '10px', padding: '8px', fontSize: '16px', border: '1px solid #000000' }}
+          style={{ color: "black", marginBottom: "10px", padding: "8px", fontSize: "16px", border: "1px solid #000000" }}
         />
         <button
           onClick={stakeCoin}
           className="p-3 bg-black text-white rounded"
-          style={{ border: '2px solid #000000' }}
+          style={{ border: "2px solid #000000" }}
         >
           Stake It
         </button>
       </div>
-  
-      <div style={{ border: '2px solid #000000', padding: '10px', marginBottom: '20px' }}>
-        <label style={{ fontSize: '18px', color: '#000000' }}>Patience pays off! Just one more minute to go before you seize the moment and withdraw. 🚀💸</label>
+
+      <div style={{ border: "2px solid #000000", padding: "10px", marginBottom: "20px" }}>
+        <label style={{ fontSize: "18px", color: "#000000" }}>
+          Patience pays off! Just one more minute to go before you seize the moment and withdraw. 🚀💸
+        </label>
         <br />
         <button
           onClick={withdrawCoin}
           className="p-3 bg-black text-white rounded"
-          style={{ border: '2px solid #000000' }}
+          style={{ border: "2px solid #000000" }}
         >
           Withdraw
         </button>
